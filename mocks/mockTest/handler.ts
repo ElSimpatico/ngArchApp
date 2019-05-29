96e416ac89467f05bc86c9b62f7724ac7193753c
Aaron Velasco <tiquismiquisss@gmail.com>
2019-05-29 23:53:04 +0200

    json-server

--- mocks/mockTest/handler.ts
+++
@@ -1,9 +0,0 @@
-const data = require('./dataJson/data');
-module.exports = (server) => {
-    server.get('/rest/api/test', (req, res) => {
-        console.log('/rest/api/test');
-        console.log(req);
-        const status = 200;
-        setTimeout(() => res.status(status).json(data), 1000);
-    });
-}
--- server/dev.server.ts
+++ server/dev.server.ts
@@ -1,33 +1,12 @@
-const glob = require('glob');
-const path = require('path');
 const jsonServer = require('json-server');
 
 const server = jsonServer.create();
-const router = jsonServer.router('router.json');
+const router = jsonServer.router('../mocks/data.json');
 const middlewares = jsonServer.defaults();
 
 server.use(middlewares);
-
-const workSpace = process.cwd();
-const handlersPath = glob.sync(path.resolve(workSpace, 'mocks/**/handler.ts'));
-
-handlersPath.forEach(handlerPath => {
-    require(handlerPath)(server);
-});
-
-server.use(jsonServer.bodyParser);
-server.use((req, res, next) => {
-    if (req.status !== 404) {
-        return next();
-    }
-    return res.status(404).json({
-        errorCode: 'endpoint.not.found',
-        message: 'The endpoint you want to access has not associated resource'
-    });
-});
-
 server.use(router);
-server.listen(4200, (err) => {
+server.listen(3000, (err) => {
     if (err) {
         console.error('[ERROR] server start', err);
     } else {
